dc060c33a65aebe81cd5a50a6c13676c17995909
son_nguyen <sonnguyenhoang127@gmail.com>
2025-06-06 00:30:32 +0700

    optimize

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Outfit } from 'next/font/google';
 import './globals.css';
 
-import { SidebarProvider } from '@/context/SidebarContext';
-import { ThemeProvider } from '@/context/ThemeContext';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -16,9 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
-          <ThemeProvider>
-            <SidebarProvider>{children}</SidebarProvider>
-          </ThemeProvider>
+            {children}
       </body>
     </html>
   );
